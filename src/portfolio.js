b42f3d049e098b7fc8e9a32d6f5a593316b0385f
Mercycermy <mercycermy5@gmail.com>
2025-01-23 00:07:05 +0300

    project update

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const about = {
   name: 'Mihret Tsegaye',
   role: 'Front-End Developer ',
   description:
-    'Ambitious front-end developer with hands-on experience in building digital solutions. A Computer Science student at CPU College. Currently expanding back-end development skills through the ALX Africa program. Passionate about developing digital solutions, especially in agriculture and education, and dedicated to supporting others.',
+    'Ambitious front-end developer with hands-on experience in building digital solutions. A Computer Science student at CPU College. Currently expanding back-end development skills through the ALX Africa program.',
   resume: 'https://docs.google.com/document/d/1z-wIBF6diRUiCs7-wRwQLNkUtu-aWaHuRMNPrunVqU8/edit?usp=sharing', // Update with actual resume link if available
   social: {
     linkedin: 'https://www.linkedin.com/in/mihret-tsegaye-nega/', // Update with actual LinkedIn profile link
@@ -19,26 +19,42 @@ const projects = [
   {
     name: 'Portfolio Website',
     description:
-      'A personal portfolio showcasing my projects and technical skills, built to demonstrate my UI/UX expertise and web development abilities.',
-    stack: ['HTML', 'CSS', 'JavaScript'],
-    sourceCode: 'https://github.com/Mercycermy/portfolio-website', // Update with actual GitHub repo link
-    livePreview: 'https://mihrettsegaye.github.io/portfolio', // Update with actual live preview link
+      'This is a personal website for Akilas Amebaw, CEO of GEDL Studio and Co-founder of Divine Pictures, designed using React.js and JSX. The site includes sections such as Home, About Us, Programs, Client Testimonials, and a Contact Form. It highlights Akilas’ career as an experimental filmmaker, showcasing his experience in TV ads, documentaries, and TV series like Tikur Ababa and Adrash. The website also features client testimonials, a portfolio of projects, and a contact page with his professional details for potential collaborations. The project demonstrates skills in creating dynamic, responsive UI elements and handling user input in React.',
+    stack: ['React'],
+    sourceCode: 'https://github.com/Mercycermy/filmakerweb', // Update with actual GitHub repo link
+    livePreview: 'https://mercycermy.github.io/filmakerweb/', // Update with actual live preview link
   },
   {
-    name: 'Local Printing Company Website Revamp',
+    name: 'Pharmacy Website',
     description:
-      'Collaborated with a local printing company to enhance their online presence by creating a responsive, user-friendly design that increased user engagement.',
-    stack: ['HTML', 'CSS', 'JavaScript', 'UI/UX'],
-    sourceCode: 'https://github.com/Mercycermy/maleda', // Placeholder link
-    livePreview: 'https://printingcompany.com', // Placeholder link
+      'This is a React.js website for Empire Pharmacy, offering pharmacy products, prescription delivery, health tracking, and expert guidance. It features a contact form powered by Node.js for easy communication, and aims to provide convenient, trusted, and community-focused healthcare services.',
+    stack: ['React'],
+    sourceCode: 'https://github.com/Mercycermy/PharmacyWeb', // Placeholder link
+    livePreview: 'https://mercycermy.github.io/PharmacyWeb/', // Placeholder link
   },
   {
-    name: ' Hackathon Projrct',
+    name: ' Hackathon Project',
     description:
       'A React-based web application offering comprehensive e-commerce solutions. Features include product listings, AI-powered product recommendations, credit and loan services, advanced inventory management, real-time data analytics, and a feedback system for transparency. Built with React and styled using Material-UI for a sleek and responsive design.',
     stack: [ 'React Framework', 'React', 'UI/UX'],
-    sourceCode: 'https://github.com/Mercycermy/maleda', // Placeholder link
-    livePreview: 'https://printingcompany.com', // Placeholder link
+    sourceCode: 'https://github.com/Mercycermy/ehl-frontend-main', // Placeholder link
+    livePreview: 'https://printingcompany', // Placeholder link
+  },
+  {
+    name: 'Mobile App Design in Figma',
+    description:
+      'An innovative and visually appealing mobile app design created using Figma. This project showcases a modern and intuitive user interface tailored for mobile devices. It includes features such as smooth navigation, user-friendly controls, and responsive layouts that ensure a seamless experience across various screen sizes and orientations.',
+    stack: [ 'Figma', 'UI/UX'],
+    sourceCode: 'https://www.figma.com/design/d5QASqjAXhWyl1yhavGMk1/InspireStart?node-id=0-1&m=dev&t=Gmf1gaZQKu31iKgw-1', // Placeholder link
+    livePreview: 'https://www.figma.com/proto/d5QASqjAXhWyl1yhavGMk1/InspireStart?node-id=0-1&t=Gmf1gaZQKu31iKgw-1', // Placeholder link
+  },
+  {
+    name: ' Pharmacy Website UI Design',
+    description:
+      'This Figma design features a clean, user-friendly homepage for Empire Pharmacy, showcasing key services, products, and reasons to choose the pharmacy. It includes interactive elements for easy navigation and quick access to contact options.',
+    stack: [ 'Figma', 'UI/UX'],
+    sourceCode: 'https://www.figma.com/design/lrCDDVNcWzReOZOWmjpKxD/Pharmacy?node-id=19-372', // Placeholder link
+    livePreview: 'https://www.figma.com/design/lrCDDVNcWzReOZOWmjpKxD/Pharmacy?node-id=19-372', // Placeholder link
   },
 ]
 
